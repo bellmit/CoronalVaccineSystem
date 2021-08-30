00085060af1dd347fb2b2312db2f3c33436aa2b4
hcj <19927539545@163.com>
2021-08-30 14:05:16 +0800

    第一次提交：整体后台Springboot+Mybatis+MySQL基本搭建完毕

--- src/main/java/com/cj/controller/VolunteersInformationController.java
+++ src/main/java/com/cj/controller/VolunteersInformationController.java
@@ -16,6 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/volunteers-information")
 public class VolunteersInformationController {
-
+//这部分等后续有时间
 }
 
